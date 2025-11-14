49649179904179faa7a006dc3342e3c528d2faea
Jessie Gautherot <jessie.gautherot@gmail.com>
2025-11-14 13:11:18 +0100

    add base.html and home.html

--- Mediatheque/settings.py
+++ Mediatheque/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Mediatheque.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
